b3e1a49ed1edc628b8b7f070ae02b64829243684
magicdog <80099856@qq.com>
2017-05-17 15:57:22 +0800

    update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by magicdog on 2017/5/17.
  */
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         System.out.println("hello world!");
