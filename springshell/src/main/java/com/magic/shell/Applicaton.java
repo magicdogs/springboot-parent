a05677fee864830ee41fe5df25235b34acd07006
magicdog <80099856@qq.com>
2017-05-18 14:54:12 +0800

    add springshell module in

---
+++ springshell/src/main/java/com/magic/shell/Applicaton.java
@@ -0,0 +1,14 @@
+package com.magic.shell;
+
+import org.springframework.shell.Bootstrap;
+
+import java.io.IOException;
+
+/**
+ * Created by magicdog on 2017/5/18.
+ */
+public class Applicaton {
+    public static void main(String[] args) throws IOException{
+        Bootstrap.main(args);
+    }
+}
