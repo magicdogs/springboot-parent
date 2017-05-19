9bed3546627d0b088bb2a88f5ac179adcd76bf72
magicdog <80099856@qq.com>
2017-05-19 11:31:31 +0800

    es https tls module

--- elasticsearch-demo/src/main/java/com/magic/elasticsearch/Application.java
+++ elasticsearch-demo/src/main/java/com/magic/elasticsearch/Application.java
@@ -1,5 +1,6 @@
 package com.magic.elasticsearch;
 
+import com.floragunn.searchguard.ssl.SearchGuardSSLPlugin;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -23,10 +24,24 @@ public class Application {
     public static void main(String[] args) throws Exception{
 
         _logger.info("start up  , arguments: {}",1);
+
+        Settings settings = Settings.builder()
+                //.put("elasticsearch.cluster-nodes","192.168.104.101:9300")
+                //.put("elasticsearch.cluster-name","app-elk")
+                .put("searchguard.ssl.transport.enabled",true)
+                .put("searchguard.ssl.transport.keystore_filepath","sgadmin-keystore.jks")
+                .put("searchguard.ssl.transport.keystore_password","671221a7fe53860f56ca")
+                .put("searchguard.ssl.transport.truststore_filepath","truststore.jks")
+                .put("searchguard.ssl.transport.truststore_password","e4780b3a3d2a7a67514c")
+                .put("path.home","d:/tmp/logs")
+                .put("searchguard.ssl.transport.enforce_hostname_verification",false)
+                //searchguard.ssl.transport.resolve_hostname: false
+                .build();
+
         // on startup
-        TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
+        TransportClient client = new PreBuiltTransportClient(settings, SearchGuardSSLPlugin.class)
                 //.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host1"), 9300))
-                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.104.101"), 9300));
         // on shutdown
 
         XContentBuilder builder = XContentFactory.jsonBuilder()
